8d9c246abbf526b5f259b3e964196ecff0ab3a11
donko1 <zalbertic@mail.ru>
2025-04-06 00:49:41 +0300

    Adding tests for IsParallel function

---
+++ __tests__/gettings_settings.test.ts
@@ -0,0 +1,10 @@
+
+import isParallel from "@/utils/get_settings"
+import expect from "@jest/globals"
+
+test("Tests if works IsParallel function", () => {
+    process.env.PARALLEL_SITE = "1"
+    expect(isParallel()).toBe(true)
+    process.env.PARALLEL_SITE = "0"
+    expect(isParallel()).toBe(false)
+})
